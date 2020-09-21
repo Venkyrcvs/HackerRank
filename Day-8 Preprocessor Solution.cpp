7118269f5194bf479136970eb1e3fc9ac8ac52f6
Venkatesh Rajendran <rcvenky@yahoo.com>
2020-09-21 19:34:14 +0530

    Day-8 Added

---
+++ Day-8 Preprocessor Solution.cpp
@@ -0,0 +1,5 @@
+#define toStr(x) #x
+#define io(v) cin >> v
+#define INF 100000000
+#define foreach(v, i) for(int i = 0; i < v.size(); i++)
+#define FUNCTION(name, op) void name(int& a, int b) { if (b op a) a = b; }
